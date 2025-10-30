0f236ca07b7783414823068125686491d64adfb2
Kamran Ayub <kamran@hey.com>
2025-10-30 11:50:40 -0500

    better desc for echo

--- demo-m2/Globomantics.Mcp.Server/EchoTool.cs
+++ demo-m2/Globomantics.Mcp.Server/EchoTool.cs
@@ -4,7 +4,14 @@ using ModelContextProtocol.Server;
 [McpServerToolType]
 public class EchoTool
 {
-    [McpServerTool, Description("Echo back the provided message")]
+    [McpServerTool, Description(
+        """
+        Echo back the provided message.
+
+        - If the user provides a repeat count, the message is repeated that many times.
+        - If they use exact quotes, then pass that as the message.
+        - If there are multiple quotes, combine them into a single message.
+        """)]
     public static string EchoMessage(
         [Description("The message to echo back")]
         string message,
